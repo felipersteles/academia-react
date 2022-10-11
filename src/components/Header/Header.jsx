96caa25fe624e0869d5aa3edc4cee80af6b2ebe3
Felipe Teles <55213753+felipersteles@users.noreply.github.com>
2022-10-10 21:24:27 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,9 @@ import Logo from '../../assets/logo.svg'
 const Header = () => {
   return (
       <div className='header'>
+          <a href="https://felipersteles.herokuapp.com/" target="_blank">
           <img src={Logo} alt='Logo' className='logo'></img>
-
+          </a>
           <ul className='header-menu'>
               <li>Pagina principal</li>
               <li>Programas</li>
